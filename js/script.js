a9b85eb0d2a98f246bfd34231a2b9f2e79ba9c78
sandroda9 <danner.sandro@bluewin.ch>
2025-10-23 21:23:18 +0200

    Implement navbar collapse on link click
    Add click event listener to navbar links for collapse functionality.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+<script>
+document.querySelectorAll('.navbar-collapse a').forEach(function(navLink) {
+  navLink.addEventListener('click', function() {
+    const navbar = document.querySelector('.navbar-collapse');
+    if (navbar.classList.contains('show')) {
+      const bsCollapse = new bootstrap.Collapse(navbar, { toggle: true });
+    }
+  });
+});
+</script>
